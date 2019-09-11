3f46da7ff5bcaf11d655889fc2c70c31a1df5d42
Alexey Cherkashin <goodniceweb@gmail.com>
2019-09-11 17:25:17 +0300

    Bump up version; Remove config which should live per repo

--- vue.js
+++ vue.js
@@ -10,25 +10,5 @@ module.exports = {
     "max-len": ["error", { "code": 120 , "ignoreUrls": true }],
     "camelcase": 0
   },
-  "settings": {
-    "import/resolver": {
-      "node": {
-        // For using absolute path for our code base
-        "path": ["src"],
-        // Please don't forget to update `vue.config.js` in your project with the following configuration:
-        //
-        // const path = require('path');
-        //
-        // module.exports = {
-        //   configureWebpack: {
-        //     resolve: {
-        //       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-        //     },
-        //   },
-        // };
-        "extensions": [".js", ".jsx", ".vue"],
-      },
-    },
-  },
 };
 
