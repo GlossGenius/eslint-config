e2f3e12b566b9591929d0319c12cafe0b6ed5244
Alexey Cherkashin <goodniceweb@gmail.com>
2019-09-11 17:21:51 +0300

    Final fixes for vue config

--- vue.js
+++ vue.js
@@ -7,6 +7,28 @@ module.exports = {
   ],
   "parser": "vue-eslint-parser",
   "rules": {
+    "max-len": ["error", { "code": 120 , "ignoreUrls": true }],
     "camelcase": 0
-  }
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        // For using absolute path for our code base
+        "path": ["src"],
+        // Please don't forget to update `vue.config.js` in your project with the following configuration:
+        //
+        // const path = require('path');
+        //
+        // module.exports = {
+        //   configureWebpack: {
+        //     resolve: {
+        //       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+        //     },
+        //   },
+        // };
+        "extensions": [".js", ".jsx", ".vue"],
+      },
+    },
+  },
 };
+
